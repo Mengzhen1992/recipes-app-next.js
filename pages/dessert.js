3571e6490a038b4f5a4afa4974369155bcf06091
Mengzhen Liao <liaomengzhen@gmail.com>
2022-10-04 17:05:33 +0200

    add image from external domain

--- pages/dessert.js
+++ pages/dessert.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Dessert() {
   return (
@@ -7,6 +8,13 @@ export default function Dessert() {
         <title>Recipes Dessert</title>
       </Head>
       <h1>This is Dessert page</h1>
+      <Image
+        src="https://unsplash.com/photos/surQ2mkZNxw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mzd8fGRlc3NlcnR8ZW58MHwwfHx8MTY1OTMzNzk1NQ&force=true&w=2400"
+        alt="dessert"
+        layout="responsive"
+        width={2400}
+        height={1599}
+      />
     </>
   );
 }
