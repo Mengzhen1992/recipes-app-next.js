5f7e84ba361b743bb49dd59fefdaa838de43770e
Mengzhen Liao <liaomengzhen@gmail.com>
2022-10-04 16:25:50 +0200

    project setup and add dessert and veggie pages

---
+++ pages/dessert.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Dessert() {
+  return (
+    <>
+      <Head>
+        <title>Recipes Dessert</title>
+      </Head>
+      <h1>This is Dessert page</h1>
+    </>
+  );
+}
---
+++ pages/veggie.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Veggie() {
+  return (
+    <>
+      <Head>
+        <title>Recipes Veggie</title>
+      </Head>
+      <h1>This is Veggie page</h1>
+    </>
+  );
+}
